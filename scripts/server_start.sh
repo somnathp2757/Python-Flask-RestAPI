4cd7b1b90a9151f5f77ea62abbcdf5cfc86fa59f
00farhan00 <112959835+00farhan00@users.noreply.github.com>
2023-06-23 14:22:20 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/server
-sudo java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+cd /home/ubuntu/server
+sudo python3 -m flask run --host=0.0.0.0
